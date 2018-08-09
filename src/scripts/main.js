c2049cb8e344bbaf7c27c13c97288f394060c378
Tinh-Pham <mrkolikz@gmail.com>
2018-08-09 13:23:23 +0700

    Add header and footer, Modifle Block main none Responsive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 console.log('FullStack Vietnam')
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop()>100) {
+            $('.nav-link.btn').addClass('hienthi');
+        }
+        else {
+            $('.nav-link.btn').removeClass('hienthi');
+        }
+    })
+    $(window).scroll(function () {
+        if ($(window).scrollTop()>100) {
+            $('.navbar.fixed-top.header_tinh').addClass('bg-new');
+        }
+        else {
+            $('.navbar.fixed-top.header_tinh').removeClass('bg-new');
+        }
+    })
+})
