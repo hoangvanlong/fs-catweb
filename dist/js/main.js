8049d989d46ab354c4ffa01aea03aedab004f98c
Tinh-Pham <mrkolikz@gmail.com>
2018-08-09 13:35:18 +0700

    no message

---
+++ dist/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+console.log('FullStack Vietnam');
+$(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $('.nav-link.btn').addClass('hienthi');
+        } else {
+            $('.nav-link.btn').removeClass('hienthi');
+        }
+    });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $('.navbar.fixed-top.header_tinh').addClass('bg-new');
+        } else {
+            $('.navbar.fixed-top.header_tinh').removeClass('bg-new');
+        }
+    });
+});
+//# sourceMappingURL=main.js.map
